c12d0e4d8572b5716c463e300ecf0b2c021e7e25
QIANGLU <598505651@qq.com>
2017-01-10 11:28:14 +0800

    抽取配置

--- ecej-parent/ecej-service/src/main/java/com/ecej/boot/demo/config/DBConfiguration.java
+++ ecej-parent/ecej-service/src/main/java/com/ecej/boot/demo/config/DBConfiguration.java
@@ -1,22 +1,17 @@
 package com.ecej.boot.demo.config;
 
 import java.sql.SQLException;
-import java.util.Properties;
 
-import org.apache.ibatis.plugin.Interceptor;
 import org.apache.ibatis.session.SqlSessionFactory;
-import org.mybatis.spring.SqlSessionFactoryBean;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import com.alibaba.druid.pool.DruidDataSource;
 import com.ecej.nove.base.config.AbstractDruidDBConfig;
-import com.github.pagehelper.PageHelper;
 
 /**
  * 核心配置，配置数据源 事物 sqlsession
@@ -45,21 +40,7 @@ public class DBConfiguration extends AbstractDruidDBConfig {
 
 	@Bean(name = "demoSqlSessionFactory")
 	public SqlSessionFactory sqlSessionFactory() throws Exception {
-		SqlSessionFactoryBean sqlSessionFactoryBean = new SqlSessionFactoryBean();
-		sqlSessionFactoryBean.setDataSource(dataSource());
-		// mybatis分页
-		PageHelper pageHelper = new PageHelper();
-		Properties props = new Properties();
-		props.setProperty("dialect", "mysql");
-		props.setProperty("reasonable", "true");
-		props.setProperty("supportMethodsArguments", "true");
-		props.setProperty("returnPageInfo", "check");
-		props.setProperty("params", "count=countSql");
-		pageHelper.setProperties(props); // 添加插件
-		sqlSessionFactoryBean.setPlugins(new Interceptor[] { pageHelper });
-		PathMatchingResourcePatternResolver resolver = new PathMatchingResourcePatternResolver();
-		sqlSessionFactoryBean.setMapperLocations(resolver.getResources("classpath:mybatis/*.xml"));
-		return sqlSessionFactoryBean.getObject();
+		return super.sqlSessionFactory(dataSource());
 	}
 
 	@Bean
